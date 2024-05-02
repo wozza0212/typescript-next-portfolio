f66b5a325b1e0b46756d09e67b920c96eadda0b5
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2024-05-02 19:59:26 +0100

    display dummy posts

--- typescript-blog-portfolio/src/app/page.tsx
+++ typescript-blog-portfolio/src/app/page.tsx
@@ -1,13 +1,42 @@
+import BlogPage from "@/components/Blog/blog-page.tsx";
 import BaseLayout from "@/components/Layouts/base-layout";
-import NavBar from "@/components/NavBar/navbar";
-import Image from "next/image";
-import Link from "next/link";
+
+type BlogPost = {
+  title: string;
+  description: string;
+  date: string | null;
+//   content: string | null;
+};
+
+const mockposts: BlogPost[] = [
+  {
+      title: "My first post",
+      description: "This is my first post",
+      date: "2024-05-01"
+  },
+  {
+      title: "My second post",
+      description: "This is my second post",
+      date: "2024-05-01"
+  },
+  {
+      title: "My third post",
+      description: "This is my third post",
+      date: "2024-05-01"
+  },
+  {
+      title: "My fourth post",
+      description: "This is my fourth post",
+      date: "2024-05-01"
+  }
+]
 
 export default function Home() {
   return (
     <BaseLayout >
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold">Welcome to my Site</h1>
+      <BlogPage mockposts={mockposts}/>
       <p className="text-xl">
         This is my blog where I write about web development and other things I
         find interesting.
