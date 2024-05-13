826e31fcd760f6f903e16feeb7985164a9ddd312
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2024-05-13 21:53:28 +0100

    add image

--- typescript-blog-portfolio/src/app/page.tsx
+++ typescript-blog-portfolio/src/app/page.tsx
@@ -1,5 +1,6 @@
 import BlogList from "@/components/Blog/blog-list";
 import BaseLayout from "@/components/Layouts/base-layout";
+import Image from "next/image";
 import { getBlogs } from "../../lib/blogs";
 import { NextPage } from "next";
 import { Blog } from "../../interfaces/Blog";
@@ -13,7 +14,18 @@ const Home: NextPage<BlogPageProps> = () => {
   return (
     <BaseLayout>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-6xl font-bold">Welcome to my Site</h1>
+        <h1 className="text-6xl font-bold">Tech Lark</h1>
+        <br />
+        <div className="h-64 w-96 relative">
+          <Image 
+            src='/Images/Main/main-image.jpg'
+            alt="Main Image"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-lg"
+            />
+        </div>
+        <br />
         <div>
           <BlogList blogs={blogs} />
         </div>
