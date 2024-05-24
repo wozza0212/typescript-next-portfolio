1822e28645bdc9022f50dfe9c2a81423bd5e3522
Thomas Wasnidge <thomas.wasnidge@sainsburys.co.uk>
2024-05-24 15:34:03 +0100

    red submit disabled

--- typescript-blog-portfolio/src/app/sandbox/form/page.tsx
+++ typescript-blog-portfolio/src/app/sandbox/form/page.tsx
@@ -7,12 +7,12 @@ import BaseLayout from "@/components/Layouts/base-layout";
 const FormPage = () => {
   const [text, setText] = useState("");
   const [btnDisabled, setBtnDisabled] = useState(true);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState<string | null>('Please enter more than 20 letters.');
 
   const unusedButton =
     "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
   const disabledButton =
-    "bg-grey-500 hover:bg-grey-700 text-white font-bold py-2 px-4 rounded";
+    "bg-red-500 hover:bg-grey-700 text-white font-bold py-2 px-4 rounded";
 
   const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if(text === '') {
@@ -21,8 +21,12 @@ const FormPage = () => {
     } else if (text !== '' && text.trim().length < 20) {
       setBtnDisabled(true);
       setMessage('Please enter more than 20 letters.')
-
     }
+    else if (text !== '' && text.trim().length > 20) {
+      setBtnDisabled(false);
+      setMessage(null)
+    }
+    
     setText(e.target.value);
   };
 
@@ -39,11 +43,14 @@ const FormPage = () => {
               placeholder="Write a review..."
               value={text}
             ></input>
-            <FormButton version={`${unusedButton}`} type={"submit"} isDisabled={btnDisabled}>
-              Submit
-            </FormButton>
+            {message && <p>{message}</p>}
+            
           </div>
-          {message && <p>{message}</p>}
+          {btnDisabled ? <FormButton version={`${disabledButton}`} type={"submit"} isDisabled={btnDisabled}>
+              Submit
+            </FormButton> : <FormButton version={`${unusedButton}`} type={"submit"} isDisabled={btnDisabled}>
+              Submit
+            </FormButton> }
         </form>
       </div>
     </BaseLayout>
