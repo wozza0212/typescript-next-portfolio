d7bfb3e94a89e478c542092d8700398fec4d77f0
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2024-06-16 16:38:49 +0100

    so far

--- typescript-blog-portfolio/src/app/sandbox/form/page.tsx
+++ typescript-blog-portfolio/src/app/sandbox/form/page.tsx
@@ -17,20 +17,48 @@ const FormPage = () => {
     "bg-gray-500 hover:bg-grey-700 text-white font-bold py-2 px-4 rounded";
 
   const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if(text === '') {
+    if (text === "") {
       setBtnDisabled(true);
-      setMessage('Please enter more than 20 letters.')
-    } else if (text !== '' && text.trim().length < 5) {
+      setMessage("Please enter more than 20 letters.");
+    } else if (text !== "" && text.trim().length < 5) {
       setBtnDisabled(true);
-      setMessage('Please enter more than 20 letters.')
-    }
-    else if (text !== '' && text.trim().length >= 5) {
+      setMessage("Please enter more than 20 letters.");
+    } else if (text !== "" && text.trim().length >= 5) {
       setBtnDisabled(false);
-      setMessage(null)
+      setMessage(null);
     }
-    
+
     setText(e.target.value);
   };
+
+  type Feedback = {
+    commentNumber: number;
+    text: string;
+    rating: number;
+  };
+
+  const baseFeedback: Feedback[] = [
+    {
+      commentNumber: 1,
+      text: "This is a great product",
+      rating: 9,
+    },
+    {
+      commentNumber: 2,
+      text: "This is a bad product",
+      rating: 1,
+    },
+    {
+      commentNumber: 3,
+      text: "This is an awesom product",
+      rating: 10,
+    },
+    {
+      commentNumber: 4,
+      text: "This is a terrible product",
+      rating: 2,
+    },
+  ];
 
   return (
     <BaseLayout>
@@ -45,18 +73,44 @@ const FormPage = () => {
               placeholder="Write a review..."
               value={text}
             ></input>
-            <div style={{paddingTop: '4px'}}>
-            {message && <p>{message}</p>}
-            {btnDisabled ? <FormButton version={`${disabledButton}`} type={"submit"} isDisabled={btnDisabled}>
-              Submit
-            </FormButton> : <FormButton version={`${unusedButton}`} type={"submit"} isDisabled={btnDisabled}>
-              Submit
-            </FormButton> }            
+            <div style={{ paddingTop: "4px" }}>
+              {message && <p>{message}</p>}
+              {btnDisabled ? (
+                <FormButton
+                  version={`${disabledButton}`}
+                  type={"submit"}
+                  isDisabled={btnDisabled}
+                >
+                  Submit
+                </FormButton>
+              ) : (
+                <FormButton
+                  version={`${unusedButton}`}
+                  type={"submit"}
+                  isDisabled={btnDisabled}
+                >
+                  Submit
+                </FormButton>
+              )}
             </div>
           </div>
-          
-            <RatingSelect select={(rating : number) => setRating(rating)}/>
+
+          <RatingSelect select={(rating: number) => setRating(rating)} />
         </form>
+        <div>
+          <h2>Feedback</h2>
+          <h4>Comments: {baseFeedback.length}</h4>
+          <ul>
+            {baseFeedback.map((feedback) => {
+              return (
+                <li key={`feedbackNum${feedback.commentNumber}`}>
+                  <p>{feedback.text}</p>
+                  <p>{feedback.rating}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </BaseLayout>
   );
