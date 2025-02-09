2869316ab20d614d78dd91b83a333b2fed731e28
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-02-09 21:41:57 +0000

    neaten up nav bar

--- typescript-blog-portfolio/src/app/page.tsx
+++ typescript-blog-portfolio/src/app/page.tsx
@@ -14,7 +14,7 @@ const Home: NextPage<BlogPageProps> = () => {
   return (
     <BaseLayout>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-6xl font-bold">Tech Lark</h1>
+        <h1 className="text-6xl font-bold">Tech Talks</h1>
         <br />
         <div className="h-100 w-200 relative">
           <Image 
