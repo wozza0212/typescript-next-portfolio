53f2f632d5fd7fe9191478f4603e39b0bcf40da7
Thomas Wasnidge <thomas.wasnidge@sainsburys.co.uk>
2025-03-05 19:37:56 +0000

    background and lnks

--- typescript-blog-portfolio/src/app/page.tsx
+++ typescript-blog-portfolio/src/app/page.tsx
@@ -24,15 +24,6 @@ const Home: NextPage<BlogPageProps | TutorialPageProps> = () => {
       <main className='flex min-h-screen flex-col items-center justify-between p-24'>
         <h1 className='text-6xl font-bold'>Tech Talks</h1>
         <br />
-        {/* <div className='h-100 w-200 relative'>
-          <Image
-            src='/Images/Main/main-image.jpg'
-            alt='Main Image'
-            height={600}
-            width={800}
-          />
-        </div> */}
-        <br />
         <div>
           <div>
             <h3>Blog Posts</h3>
--- typescript-blog-portfolio/src/components/Blog/blog-item.tsx
+++ typescript-blog-portfolio/src/components/Blog/blog-item.tsx
@@ -1,50 +1,50 @@
-import Link from "next/link";
-import Image from "next/legacy/image";
-import { FunctionComponent } from "react";
+import Link from 'next/link'
+import Image from 'next/legacy/image'
+import { FunctionComponent } from 'react'
 type Props = {
-  post: BlogPost;
-  index: number;
-};
+  post: BlogPost
+  index: number
+}
 
 type BlogPost = {
-  title: string;
-  author: string;
-  description: string;
-  date: string;
-  image: string;
-  slug: string;
-};
+  title: string
+  author: string
+  description: string
+  date: string
+  image: string
+  slug: string
+}
 
 const BlogItem: FunctionComponent<Props> = (props: Props) => {
-  const { post, index } = props;
+  const { post, index } = props
   return (
-    <div className="group">
-      <div
-        key={index}
-        className="h-80 aspect-w-1 aspect-h-1 w-full rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-40"
-      >
-        <Link href={`/blog/${post.slug}`}>
-          <div className="relative h-80 aspect-w-1 aspect-h-1 w-full rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-40">
+    <div className='group'>
+      <Link href={`/blog/${post.slug}`}>
+        <div
+          key={index}
+          className='h-80 aspect-w-1 aspect-h-1 w-full rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-40'
+        >
+          <div className='relative h-80 aspect-w-1 aspect-h-1 w-full rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-40'>
             <Image
               src={post.image}
               alt={post.title}
-              objectFit="contain"
-              layout="fill"
-              className="rounded-lg hover:cursor-pointer"
+              objectFit='contain'
+              layout='fill'
+              className='rounded-lg hover:cursor-pointer'
             />
           </div>
-        </Link>
-      </div>
-      <div className="mt-4 flex justify-between">
-        <div>
-          <h2 className="text-sm text-white font-bold">{post.title}</h2>
-          <h2 className="text-sm text-white font-bold">{post.author}</h2>
-          <p className="mt-1 text-sm text-gray-500">{post.date}</p>
-          <p className="mt-1 text-sm text-gray-500">{post.description}</p>
         </div>
-      </div>
+        <div className='mt-4 flex justify-between'>
+          <div>
+            <h2 className='text-lg text-black font-bold'>{post.title}</h2>
+            <h2 className='text-sm text-white font-bold'>{post.author}</h2>
+            <p className='mt-1 text-md text-gray-900'>{post.date}</p>
+            <p className='mt-1 text-md text-gray-900'>{post.description}</p>
+          </div>
+        </div>
+      </Link>
     </div>
-  );
-};
+  )
+}
 
-export default BlogItem;
+export default BlogItem
