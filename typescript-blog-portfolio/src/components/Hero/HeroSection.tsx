0ed30df6c0e0886692dc419f2628b9f2a18ca1c6
Thomas Wasnidge <thomas.wasnidge@sainsburys.co.uk>
2025-03-12 21:52:49 +0000

    wip

--- typescript-blog-portfolio/src/components/Hero/HeroSection.tsx
+++ typescript-blog-portfolio/src/components/Hero/HeroSection.tsx
@@ -7,21 +7,25 @@ import styles from './hero.module.css'
 const Hero: FunctionComponent = () => {
   return (
     <div className={styles.heroContainer}>
-      <div className='text-lg text-black'>
-        <h2>Combining Design with Application</h2>
-        <p>I`m A</p>
-        <h2>Full Stack Software Engineer</h2>
-        <div>
-          <p>
-            This website is being created with the sole purpose to help{' '}
-            <Link href='/about'>me</Link> develop my skills as a softare
-            engineer, play with some design features that I dont always get to
-            do at work, and hopefully to help teach and inspire others to begin
-            or develop their software engineer career
-          </p>
+      <div className={styles.textBox}>
+        <div className='text-lg text-black'>
+          <h2 className={styles.heroHeading}>
+            Combining Design with Application<p>I`m A</p>
+          </h2>
+
+          <h2 className={styles.fullStack}>Full Stack Software Engineer</h2>
+          <div>
+            <p className={styles.thisSitePara}>
+              This website is being created with the sole purpose to help{' '}
+              <Link href='/about'>me</Link> develop my skills as a softare
+              engineer, play with some design features that I dont always get to
+              do at work, and hopefully to help teach and inspire others to
+              begin or develop their software engineer career
+            </p>
+          </div>
         </div>
-        <PhotoPlaceholder width={500} height={500} />
       </div>
+      <PhotoPlaceholder className={styles.heroImage} width={650} height={650} />
     </div>
   )
 }
